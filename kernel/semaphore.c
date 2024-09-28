e64615f482532867ac6dc1c7f8123bfb7f4fa854
Jx <103049321+JxJxxJxJ@users.noreply.github.com>
2024-09-28 17:57:15 -0300

    formateo

--- kernel/semaphore.c
+++ kernel/semaphore.c
@@ -8,39 +8,56 @@
 #include "defs.h"
 #include "stdbool.h"
 
-#define MAX_SEM            500  // Cantidad máxima de semáforos
-                                // disponibles.
-#define ERROR_CODE           0
-#define SUCCESS_CODE         1
-#define CLOSED_SEM_VALUE    -1  // Optamos por tomar al valor
-                                // -1 para indicar que el semáforo
-                                // está cerrado.
+/*
+    Cantidad máxima de semáforos
+    disponibles.
+*/
+#define MAX_SEM            500  
 
-// Todas estas syscalls realmente están modificando
-// una entrada en la SEM_ARRAY. Es importante que, como
-// RISC-V es multicore, esta modificación se haga de forma
-// atómica. XV6's locks enter the game!
+/*
+    Optamos por tomar al valor
+    -1 para indicar que el semáforo
+    está cerrado.
+*/
+#define CLOSED_SEM_VALUE    -1  
 
-// Voy a usar locks para proteger una sección crítica, que
-// va a ser cuando accedo a las entries del arreglo de semá-
-// foros. Se dice que el lock va a proteger a la entry del
-// arreglo.
+/*
+    Todas estas syscalls realmente están modificando
+    una entrada en la SEM_ARRAY. Es importante que, como
+    RISC-V es multicore, esta modificación se haga de forma
+    atómica. XV6's locks enter the game.
+    
+    Voy a usar locks para proteger una sección crítica, que
+    va a ser cuando accedo a las entries del arreglo de semá-
+    foros. Se dice que el lock va a proteger a la entry del
+    arreglo.
+*/
 
-// Estructura del semáforo.
+/*
+    Estructura del semáforo.
+*/
 typedef struct sem_s {
   struct spinlock lock;
   int value;
   bool is_active;
 } semaphore;
 
+/*
+    Se implementan a los semáforos como un arreglo dentro
+    del espacio de kernel.
+*/
+/*
+    INVARIANTE (*): El usuario solo puede acceder a los semaforos 
+    entre los indices [0] y [MAX_SEM]. El ultimo semaforo se usa
+    para sincronizar a la funcion sem_find_free_channel_and_set()
+    cuando muchos procesos la llaman al mismo tiempo.
+*/
+semaphore sem_array[MAX_SEM+1];
 
-// Se implementan a los semáforos como un arreglo dentro
-// del espacio de kernel.
-semaphore sem_array[MAX_SEM];
-
-
-// Inicializa el arreglo global de semáforos `sem_array`,
-// seteando todos los valores en -1.
+/*
+    Inicializa el arreglo global de semáforos `sem_array`,
+    seteando todos los valores en CLOSED_SEM_VALUE (-1).
+*/
 uint64
 sem_init_array(void)
 {
@@ -51,9 +68,18 @@ sem_init_array(void)
   return SUCCESS_CODE;
 }
 
+/*
+    Busca el primer semáforo inactivo en el arreglo de
+    semáforos `sem_array`. Al hallarlo, abre ese semáforo
+    e inicializa su valor en 0.
+*/
 uint64
-sem_find_free_channel_and_set(int value)
+sem_find_free_channel_and_set(int value) 
 {
+  // Adquiero el lock especializado
+  acquire(&sem_array[MAX_SEM].lock);
+
+  // ----------- ZONA CRITICA -----------
   bool index_found = false;
   int index = CLOSED_SEM_VALUE;
   for (int i = 0; i < MAX_SEM && !index_found; i++){
@@ -61,59 +87,22 @@ sem_find_free_channel_and_set(int value)
         // skip
     }
     if (!sem_array[i].is_active){
-
-      // Adquiero el lock del semaforo sem
-      // acquire(&sem_array[i].lock);
-
-      // ----------- ZONA CRITICA -----------
-
-      // sem_array[i].value = value;
-      // sem_array[i].is_active = true;
-
       sem_open(i,value);
-
       index = i;
       index_found = true;
-
-      // -------- FIN ZONA CRITICA -----------
-
-      // Devuelvo el lock del semaforo sem
-      // release(&sem_array[i].lock);
     }
   }
-
-  // return index_found ? index : ERROR_CODE;
+  // -------- FIN ZONA CRITICA -----------
+  
+  // Devuelvo el lock del semaforo sem
+  release(&sem_array[MAX_SEM].lock);
   return index;
 }
 
-
-
-// Busca el primer semáforo inactivo en el arreglo de
-// semáforos `sem_array`. Al hallarlo, abre ese semáforo
-// e inicializa su valor en 0.
-uint64
-sem_find_free_channel(void)
-{
-  bool index_found = false;
-  int index = 100000;
-  for (int i = 0; i < MAX_SEM && !index_found; i++){
-    if (sem_array[i].is_active){
-        // skip
-    }
-    if (!sem_array[i].is_active){
-      sem_open(i,0);
-      sem_array[i].is_active = true;
-      index = i;
-      index_found = true;
-    }
-  }
-
-  return index_found ? index : ERROR_CODE;
-}
-
-
-// Abre un semáforo dado su índice `sem` y lo inicializa
-// con el valor `value` proporcionado.
+/*
+    Abre un semáforo dado su índice `sem` y lo inicializa
+    con el valor `value` proporcionado.
+*/
 uint64
 sem_open(int sem, int value)
 {
@@ -122,37 +111,32 @@ sem_open(int sem, int value)
   bool error = false;
 
   // ------------------ ZONA CRITICA ------------------
-
   if (sem < 0 || sem >= MAX_SEM){ // Si estoy fuera de los indices
-    printf("ERROR: Indice fuera del arreglo de semaforos. (%d) debe estar entre 0 y (%d)\n", sem, MAX_SEM);
     error = true;
   }
   if (0 <= sem && sem < MAX_SEM){ // Si estoy dentro de los indices
     if(value < 0){                   // Pero le doy un valor negativo
-      printf("ERROR: No se puede abrir un semaforo con un valor < 0. (%d)\n", value);
       error = true;
     }
     if(sem_array[sem].is_active){    // Pero el semaforo esta activado
-      printf("ERROR: No se puede abrir un semaforo que ya esta activado. (sem[%d]=%d)\n", sem, value);
       error = true;
     }
     if (!error){                     // Y todo esta bien
-      // printf("Se ejecuta semopen\n");
       sem_array[sem].value = value;
       sem_array[sem].is_active = true;
     }
-
+  }
   // ---------------- FIN ZONA CRITICA ----------------
 
-    // Devuelvo el lock del semáforo sem.
-    release(&(sem_array[sem].lock));
-  }
+  // Devuelvo el lock del semáforo sem.
+  release(&(sem_array[sem].lock));
   return error ? ERROR_CODE : SUCCESS_CODE;
 }
 
-
-// Libera el semáforo en la posición `sem` del arreglo
-// `sem_array`.
+/*
+    Libera el semáforo en la posición `sem` del arreglo
+    `sem_array`.
+*/
 uint64
 sem_close(int sem)
 {
@@ -161,25 +145,19 @@ sem_close(int sem)
   bool error = false;
 
   // ------------------ ZONA CRITICA ------------------
-
   if (sem < 0 || sem >= MAX_SEM){ // Si estoy fuera de los indices
     printf("ERROR: Indice fuera del arreglo de semaforos. (%d) debe estar entre 0 y (%d)\n", sem, MAX_SEM);
     error = true;
   }
-
   if (0 <= sem && sem < MAX_SEM){ // Si estoy dentro de los indices
-    // if(!sem_array[sem].is_active){    // Pero el semaforo no esta activado
-    //   printf("ERROR: No se puede cerrar un semaforo que ya esta desactivado. \n");
-    //   error = true;
-    // }
+    if(!sem_array[sem].is_active){    // Pero el semaforo no esta activado
+      error = true;
+    }
   }
-
   if (!error){                        // Y todo esta bien
-    // printf("Se ejecuta semclose\n");
     sem_array[sem].value = CLOSED_SEM_VALUE;
     sem_array[sem].is_active = false;
   }
-
   // ---------------- FIN ZONA CRITICA ----------------
 
   // Devuelvo el lock del semáforo sem.
@@ -187,10 +165,11 @@ sem_close(int sem)
   return error ? ERROR_CODE : SUCCESS_CODE;
 }
 
-
-// Incrementa el valor del semáforo en la posición `sem`
-// del arreglo `sem_array`. Si el valor del semáforo es 0,
-// desbloquea los procesos.
+/*
+    Incrementa el valor del semáforo en la posición `sem`
+    del arreglo `sem_array`. Si el valor del semáforo es 0,
+    desbloquea los procesos.
+*/
 uint64
 sem_up(int sem)
 {
@@ -199,15 +178,13 @@ sem_up(int sem)
   bool error = false;
 
   // ------------------ ZONA CRITICA ------------------
-
   if (sem < 0 || sem >= MAX_SEM){
-    printf("ERROR: Índice fuera del arreglo de semáforos. (%d) debe estar entre 0 y (%d)\n", sem, MAX_SEM);
-    return ERROR_CODE;
+    error = true;
   }
-
-  sem_array[sem].value++;
-  wakeup(&(sem_array[sem]));
-
+  if (!error){
+    sem_array[sem].value++;
+    wakeup(&(sem_array[sem]));
+  }
   // ---------------- FIN ZONA CRITICA ----------------
 
   // Devuelvo el lock del semáforo sem.
@@ -216,10 +193,11 @@ sem_up(int sem)
 
 }
 
-
-// Decrementa el semáforo ”sem” bloqueando los procesos
-// cuando su valor es 0. El valor del semáforo nunca puede
-// ser menor a 0.
+/*
+     Decrementa el semáforo ”sem” bloqueando los procesos
+     cuando su valor es 0. El valor del semáforo nunca puede
+     ser menor a 0.
+*/
 uint64
 sem_down(int sem)
 {
@@ -228,17 +206,15 @@ sem_down(int sem)
   bool error = false;
 
   // ------------------ ZONA CRITICA ------------------
-
   if (sem < 0 || sem >= MAX_SEM){
-    printf("ERROR: Índice fuera del arreglo de semáforos. (%d) debe estar entre 0 y (%d)\n", sem, MAX_SEM);
-    return ERROR_CODE;
+    error = true;
   }
-
-  while (sem_array[sem].value <= 0) {
-        sleep(&(sem_array[sem]), &(sem_array[sem].lock));
+  if (!error){
+    while (sem_array[sem].value <= 0) {
+      sleep(&(sem_array[sem]), &(sem_array[sem].lock));
+    }
+    sem_array[sem].value--;
   }
-  sem_array[sem].value--;
-
   // ---------------- FIN ZONA CRITICA ----------------
 
   // Devuelvo el lock del semáforo sem.
--- kernel/semaphore.h
+++ kernel/semaphore.h
@@ -1,10 +1,60 @@
 #ifndef SEMAPHORE_H
 #define SEMAPHORE_H
 
+
+
 #include "types.h"  // me da al amigo uint64
+
+/*
+  Aca se va a definir la politica
+  
+  Algunas cosas a decir:
+  1- Las funciones fallan silenciosamente, esto es: 
+    NO usan printf, solo imprimen su codigo de error (0),
+    esto es una mala decision de diseño, algo mejor 
+    seria usar multiples codigos de error, pero no nos dejan.
+    
+  2- Que sucede cuando sem_open abre un sem ya abierto.
+    
+  3- Que sucede cuando sem_close cierra un sem ya cerrado.
+ */
+
+#define ERROR_CODE           0
+#define SUCCESS_CODE         1
+
+/*
+ * PRECONDICION:
+ * DESCRIPCION:
+ * POSTCONDICION:
+*/
 uint64    sem_open(int sem, int value);
+
+/*
+ * PRECONDICION:
+ * DESCRIPCION:
+ * POSTCONDICION:
+*/
 uint64    sem_close(int sem);
+
+/*
+ * PRECONDICION:
+ * DESCRIPCION:
+ * POSTCONDICION:
+*/
 uint64    sem_up(int sem);
+
+/*
+ * PRECONDICION:
+ * DESCRIPCION:
+ * POSTCONDICION:
+*/
 uint64    sem_down(int sem);
+
+/*
+ * PRECONDICION:
+ * DESCRIPCION:
+ * POSTCONDICION:
+*/
+uint64    sem_find_free_channel_and_set(int value);
 
 #endif 
